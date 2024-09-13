c11924aa0c1b4ba5f8e73fa92f9711a39e55486d
GitUsmanBaig <i211132@nu.edu.pk>
2024-09-14 00:16:54 +0500

    updated folder structure

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask, request, jsonify
 import joblib
+import pandas as pd
 
 app = Flask(__name__)
 
 # Load your trained model
-model = joblib.load('path_to_your_model/house_price_predictor_model.joblib')
+model = joblib.load('house_price_predictor_model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get data from Post request
         data = request.get_json()
-        # Assuming data is passed as a list of values
-        features = [data['area'], data['bedrooms'], data['bathrooms'], data['stories'],
-                    data['mainroad'], data['guestroom'], data['basement'],
-                    data['hotwaterheating'], data['airconditioning'], data['parking'],
-                    data['prefarea'], data['furnishingstatus']]
+        
+        # Convert input data into DataFrame
+        input_df = pd.DataFrame([data])
         
         # Make prediction
-        prediction = model.predict([features])[0]  # [0] to get the single value
+        prediction = model.predict(input_df)[0]  # [0] to get the single value
         
         # Respond with prediction
         return jsonify({'prediction': prediction})
